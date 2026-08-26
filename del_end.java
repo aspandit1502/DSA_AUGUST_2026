19ba6ec3e55df89dcb4e289394d7aa5f6449145a
Arya <aryashampandit@gmail.com>
2026-08-26 19:37:18 +0530

    delete ele from end of array

---
+++ del_end.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class del_end
+ {
+    public static void main(String[] args) {
+         int nums[]= {1,2,3,4,5};
+    int i=nums.length-1;
+    nums[i]=0;
+    System.out.println(Arrays.toString(nums));
+
+        
+    }
+   
+    
+}
