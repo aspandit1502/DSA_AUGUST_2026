aa928e0f046c687380fff1201e711dfb93a5b916
Arya <aryashampandit@gmail.com>
2026-09-04 11:03:56 +0530

    linear search-first occ

--- linear_sea.java
+++ linear_sea.java
@@ -1,13 +1,14 @@
-public class linear_sea
+public class linear_sea //find the first occ of the element
  {
     public static void main(String[] args) {
         int[] nums= {1,2,3,4,5,6,99,20};
          int val=99;
-        for(int i=0;i<nums.length-1;i++)
+        for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==val)
             {
-              System.out.println(i+1);
+              System.out.println(val+" is found at position "+ (i+1) +" in the array");
+              break;
             }
         }
     }
